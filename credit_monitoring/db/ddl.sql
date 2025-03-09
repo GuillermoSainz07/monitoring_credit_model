6e44a2a00f6dc0e4fe54f7402dc105da2adcf33b
GuillermoSainz07 <86427154+GuillermoSainz07@users.noreply.github.com>
2025-03-09 11:42:57 -0700

    add grafana/nannyml file.yml

---
+++ credit_monitoring/db/ddl.sql
@@ -0,0 +1,8 @@
+/*
+ * Author:   Niels Nuyttens  <niels@nannyml.com>
+ *
+ * License: Apache Software License 2.0
+ */
+
+CREATE USER nannyml WITH ENCRYPTED PASSWORD 'we<3nannyml';
+CREATE DATABASE nannyml OWNER nannyml;
